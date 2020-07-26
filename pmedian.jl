8120a19180ae06f8c0d6395c0f733457cf3569a4
odow <o.dowson@gmail.com>
2020-07-26 10:44:44 -0500

    Tidy pmedian experiments

--- pmedian.jl
+++ pmedian.jl
@@ -17,51 +17,252 @@ end
 
 # This is the LP relaxation.
 function generate_moi_problem(model, data::PMedianData)
-    facility_variables = MOI.add_variables(model, data.num_locations)
+    NL = data.num_locations
+    NC = data.num_customers
+
+    ###
+    ### 0 <= facility_variables <= 1
+    ###
+
+    facility_variables = MOI.add_variables(model, NL)
+
     for v in facility_variables
-        MOI.add_constraint(model, v, MOI.GreaterThan(0.0))
-        MOI.add_constraint(model, v, MOI.LessThan(1.0))
+        MOI.add_constraint(model, MOI.SingleVariable(v), MOI.Interval(0.0, 1.0))
     end
-    assignment_variables = [MOI.add_variable(model) for i in 1:data.num_customers, j in 1:data.num_locations]
+
+    ###
+    ### assignment_variables >= 0
+    ###
+
+    assignment_variables = reshape(MOI.add_variables(model, NC * NL), NC, NL)
     for v in assignment_variables
-        MOI.add_constraint(model, v, MOI.GreaterThan(0.0))
+        MOI.add_constraint(model, MOI.SingleVariable(v), MOI.GreaterThan(0.0))
         # "Less than 1.0" constraint is redundant.
     end
-    objective = MOI.ScalarAffineFunction(
-        [MOI.ScalarAffineTerm(abs(data.customer_locations[i] - j), assignment_variables[i,j])
-        for i in 1:data.num_customers for j in 1:data.num_locations],
-            0.0)
-    MOI.set(model, MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}(), objective)
+
+    ###
+    ### Objective function
+    ###
+
+    MOI.set(
+        model,
+        MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}(),
+        MOI.ScalarAffineFunction(
+            [
+                MOI.ScalarAffineTerm(
+                    abs(data.customer_locations[i] - j),
+                    assignment_variables[i, j]
+                )
+                for i in 1:NC for j in 1:NL
+            ],
+            0.0,
+        ),
+    )
     MOI.set(model, MOI.ObjectiveSense(), MOI.MIN_SENSE)
-    for i in 1:data.num_customers, j in 1:data.num_locations
-        # assignment_variables[i,j] <= facility_variables[j]
-        MOI.add_constraint(model, MOI.ScalarAffineFunction(
-            [MOI.ScalarAffineTerm(1.0, assignment_variables[i,j]),
-            MOI.ScalarAffineTerm(-1.0, facility_variables[j])], 0.0),
-            MOI.LessThan(0.0))
-    end
-    for i in 1:data.num_customers
-        # sum_j assignment_variables[i,j] = 1
-        MOI.add_constraint(model, MOI.ScalarAffineFunction(
-            [MOI.ScalarAffineTerm(1.0, assignment_variables[i,j]) for j in 1:data.num_locations],
-            0.0),
-            MOI.EqualTo(1.0)
+
+    ###
+    ### assignment_variables[i, j] <= facility_variables[j]
+    ###
+
+    for i in 1:NC, j in 1:NL
+        MOI.add_constraint(
+            model,
+            MOI.ScalarAffineFunction(
+                [
+                    MOI.ScalarAffineTerm(1.0, assignment_variables[i, j]),
+                    MOI.ScalarAffineTerm(-1.0, facility_variables[j])
+                ],
+                0.0,
+            ),
+            MOI.LessThan(0.0),
         )
     end
-    # sum_j facility_variables[j] = num_facilities
-    MOI.add_constraint(model, MOI.ScalarAffineFunction(
-        MOI.ScalarAffineTerm.(1.0, facility_variables), 0.0),
-        MOI.EqualTo{Float64}(data.num_facilities)
+
+    ###
+    ### sum_j assignment_variables[i, j] = 1
+    ###
+
+    for i in 1:NC
+        MOI.add_constraint(
+            model,
+            MOI.ScalarAffineFunction(
+                [
+                    MOI.ScalarAffineTerm(1.0, assignment_variables[i, j])
+                    for j in 1:NL
+                ],
+                0.0,
+            ),
+            MOI.EqualTo(1.0),
+        )
+    end
+
+    ###
+    ### sum_j facility_variables[j] == num_facilities
+    ###
+
+    MOI.add_constraint(
+        model,
+        MOI.ScalarAffineFunction(
+            MOI.ScalarAffineTerm.(1.0, facility_variables),
+            0.0,
+        ),
+        MOI.EqualTo{Float64}(data.num_facilities),
     )
+
+    return assignment_variables, facility_variables
+end
+
+function generate_moi_problem_vector(model, data::PMedianData)
+    NL = data.num_locations
+    NC = data.num_customers
+
+    ###
+    ### facility_variables    ∈ R₊
+    ### facility_variables -1 ∈ R₋
+    ###
+
+    facility_variables = MOI.add_variables(model, NL)
+    MOI.add_constraint(
+        model,
+        MOI.VectorAffineFunction(
+            [
+                MOI.VectorAffineTerm(i, MOI.ScalarAffineTerm(1.0, x))
+                for (i, x) in enumerate(facility_variables)
+            ],
+            zeros(NL),
+        ),
+        MOI.Nonnegatives(NL),
+    )
+
+    MOI.add_constraint(
+        model,
+        MOI.VectorAffineFunction(
+            [
+                MOI.VectorAffineTerm(i, MOI.ScalarAffineTerm(1.0, x))
+                for (i, x) in enumerate(facility_variables)
+            ],
+            -ones(NL),
+        ),
+        MOI.Nonpositives(NL),
+    )
+
+    ###
+    ### assignment_variables ∈ R₊
+    ###
+
+    assignment_variables = reshape(MOI.add_variables(model, NC * NL), NC, NL)
+    MOI.add_constraint(
+        model,
+        MOI.VectorAffineFunction(
+            [
+                MOI.VectorAffineTerm(
+                    i, MOI.ScalarAffineTerm(1.0, assignment_variables[i])
+                )
+                for i in eachindex(assignment_variables)
+            ],
+            zeros(NC * NL),
+        ),
+        MOI.Nonnegatives(NC * NL),
+    )
+    # "Less than 1.0" constraint is redundant.
+
+    ###
+    ### Objective function
+    ###
+
+    MOI.set(
+        model,
+        MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}(),
+        MOI.ScalarAffineFunction(
+            [
+                MOI.ScalarAffineTerm(
+                    abs(data.customer_locations[i] - j),
+                    assignment_variables[i, j],
+                )
+                for i in 1:NC for j in 1:NL
+            ],
+            0.0,
+        )
+    )
+    MOI.set(model, MOI.ObjectiveSense(), MOI.MIN_SENSE)
+
+    ###
+    ### assignment_variables[i,j] <= facility_variables[j]
+    ###     ⟺
+    ### assignment_variables[i,j] - facility_variables[j] ∈ R₋
+    ###
+
+    terms = Vector{MOI.VectorAffineTerm{Float64}}(undef, 2 * NC * NL)
+    row = 1
+    for i in 1:NC, j in 1:NL
+        terms[2 * row - 1] = MOI.VectorAffineTerm(
+            row, MOI.ScalarAffineTerm(1.0, assignment_variables[i, j])
+        )
+        terms[2 * row] = MOI.VectorAffineTerm(
+            row, MOI.ScalarAffineTerm(-1.0, facility_variables[j])
+        )
+        row += 1
+    end
+
+    MOI.add_constraint(
+        model,
+        MOI.VectorAffineFunction(terms, zeros(NC * NL)),
+        MOI.Nonpositives(NC * NL),
+    )
+
+    ###
+    ### sum_j assignment_variables[i,j] == 1
+    ###     ⟺
+    ### sum_j assignment_variables[i,j] - 1 ∈ 0
+    ###
+
+    terms_2 = Vector{MOI.VectorAffineTerm{Float64}}(undef, NC * NL)
+    k = 1
+    for i in 1:NC, j in 1:NL
+        terms_2[k] = MOI.VectorAffineTerm(
+            i, MOI.ScalarAffineTerm(1.0, assignment_variables[i, j])
+        )
+        k += 1
+    end
+    MOI.add_constraint(
+        model,
+        MOI.VectorAffineFunction(terms_2, -ones(NC)),
+        MOI.Zeros(NC),
+    )
+
+    ###
+    ### sum_j facility_variables[j] - num_facilities ∈ 0
+    ###
+
+    MOI.add_constraint(
+        model,
+        MOI.VectorAffineFunction(
+            MOI.VectorAffineTerm.(
+                1, MOI.ScalarAffineTerm.(1.0, facility_variables)
+            ),
+            Float64[-data.num_facilities],
+        ),
+        MOI.Zeros(1),
+    )
+
     return assignment_variables, facility_variables
 end
 
 function generate_glpk_problem(prob, data::PMedianData)
-    facility_variables = [GLPK.glp_add_cols(prob, 1) for i in 1:data.num_locations]
-    assignment_variables = [GLPK.glp_add_cols(prob, 1) for i in 1:data.num_customers, j in 1:data.num_locations]
-    for j in 1:data.num_locations
+    NL = data.num_locations
+    NC = data.num_customers
+
+    facility_variables = [GLPK.glp_add_cols(prob, 1) for i in 1:NL]
+
+    for j in 1:NL
         GLPK.glp_set_col_bnds(prob, facility_variables[j], GLPK.GLP_DB, 0.0, 1.0)
     end
+
+    assignment_variables = [
+        GLPK.glp_add_cols(prob, 1)
+        for i in 1:data.num_customers, j in 1:data.num_locations
+    ]
+
     for i in 1:data.num_customers, j in 1:data.num_locations
         GLPK.glp_set_col_bnds(prob, assignment_variables[i, j], GLPK.GLP_DB, 0.0, 1.0)
         GLPK.glp_set_obj_coef(prob, assignment_variables[i, j], abs(data.customer_locations[i] - j))
@@ -101,6 +302,7 @@ function generate_glpk_problem(prob, data::PMedianData)
         push!(V, 1.0)
     end
     GLPK.glp_load_matrix(prob, length(I) - 1, I, J, V)
+    return
 end
 
 function solve_glpk_direct(data::PMedianData; time_limit_sec=Inf)
@@ -112,7 +314,6 @@ function solve_glpk_direct(data::PMedianData; time_limit_sec=Inf)
         if isfinite(time_limit_sec)
             param.tm_lim = 1000 * time_limit_sec
         end
-
         @timeit "generate" generate_glpk_problem(prob, data)
         @timeit "solve" GLPK.glp_simplex(prob, param)
         objval = GLPK.glp_get_obj_val(prob)
@@ -131,7 +332,7 @@ function generate_scs_problem(data::PMedianData)
     I = Int[]
     J = Int[]
     V = Float64[]
-    
+
     num_constraints = 0
     for i in 1:data.num_customers
         # sum_j assignment_variables[i,j] = 1
@@ -200,62 +401,92 @@ end
 function solve_scs_direct(data::PMedianData; max_iters)
     @timeit "SCS direct" begin
         @timeit "generate" scs_prob = generate_scs_problem(data)
-        @timeit "solve" solution = SCS.SCS_solve(SCS.IndirectSolver, 
-                                    scs_prob...; max_iters=max_iters,
-                                    acceleration_lookback=0, verbose=0)
+        @timeit "solve" solution = SCS.SCS_solve(
+            SCS.IndirectSolver,
+            scs_prob...;
+            max_iters=max_iters,
+            acceleration_lookback=0,
+            verbose=0,
+        )
     end
-    objval = scs_prob[5]'*solution.x
+    objval = scs_prob[5]' * solution.x
     return objval
 end
 
-function solve_moi(data::PMedianData, optimizer; params)
-    model = MOI.Bridges.full_bridge_optimizer(MOI.Utilities.CachingOptimizer(
+function solve_moi(data::PMedianData, optimizer; vector_version, params)
+    cache = MOI.Utilities.CachingOptimizer(
         MOI.Utilities.UniversalFallback(MOI.Utilities.Model{Float64}()),
-        optimizer()), Float64)
+        optimizer()
+    )
+    model = MOI.Bridges.full_bridge_optimizer(cache, Float64)
+    MOI.Utilities.reset_optimizer(cache)
     for (param, value) in params
         MOI.set(model, param, value)
     end
-    @timeit "generate" x, y = generate_moi_problem(model, data)
+    @timeit "generate" x, y = if vector_version
+        generate_moi_problem_vector(model, data)
+    else
+        generate_moi_problem(model, data)
+    end
     @timeit "solve" MOI.optimize!(model)
     return MOI.get(model, MOI.ObjectiveValue())
 end
 
 
-function solve_glpk_moi(data::PMedianData; time_limit_sec=Inf)
+function solve_glpk_moi(data::PMedianData; vector_version, time_limit_sec=Inf)
     params = []
     if isfinite(time_limit_sec)
         push!(params, (MOI.TimeLimitSec(), time_limit_sec))
     end
-    @timeit "GLPK MOI" solve_moi(data, GLPK.Optimizer, params=params)
+    s_type = vector_version ? "vector" : "scalar"
+    @timeit(
+        "GLPK MOI $(s_type)",
+        solve_moi(
+            data, GLPK.Optimizer; vector_version=vector_version, params=params
+        )
+    )
 end
 
-function solve_scs_moi(data::PMedianData; max_iters)
-    params = [(MOI.RawParameter("max_iters"), max_iters),
-              (MOI.Silent(), true),
-              (MOI.RawParameter("acceleration_lookback"), 0)]
-    @timeit "SCS MOI" solve_moi(data, SCS.Optimizer, params=params)
+function solve_scs_moi(data::PMedianData; vector_version, max_iters)
+    params = [
+        (MOI.RawParameter("max_iters"), max_iters),
+        (MOI.Silent(), true),
+        (MOI.RawParameter("acceleration_lookback"), 0)
+    ]
+    s_type = vector_version ? "vector" : "scalar"
+    @timeit(
+        "SCS MOI $(s_type)",
+        solve_moi(
+            data, SCS.Optimizer; vector_version=vector_version, params=params
+        )
+    )
 end
 
-
-
-function run_benchmark(;num_facilities, num_customers, num_locations,
-        time_limit_sec, max_iters)
+function run_benchmark(;
+    num_facilities, num_customers, num_locations, time_limit_sec, max_iters
+)
     Random.seed!(10)
     reset_timer!()
     data = PMedianData(num_facilities, num_customers, num_locations, rand(num_customers) .* num_locations)
-    
-    glpk_moi_obj = solve_glpk_moi(data, time_limit_sec=time_limit_sec)
-    @show glpk_moi_obj
+
+    glpk_moi_vector_obj = solve_glpk_moi(data, vector_version=true, time_limit_sec=time_limit_sec)
+    @show glpk_moi_vector_obj
+
+    glpk_moi_scalar_obj = solve_glpk_moi(data, vector_version=false, time_limit_sec=time_limit_sec)
+    @show glpk_moi_scalar_obj
 
     glpk_direct_obj = solve_glpk_direct(data, time_limit_sec=time_limit_sec)
     @show glpk_direct_obj
 
-    scs_moi_obj = solve_scs_moi(data, max_iters=max_iters)
-    @show scs_moi_obj
+    scs_moi_vector_obj = solve_scs_moi(data, vector_version=true, max_iters=max_iters)
+    @show scs_moi_vector_obj
+
+    scs_moi_scalar_obj = solve_scs_moi(data, vector_version=false, max_iters=max_iters)
+    @show scs_moi_scalar_obj
 
     scs_direct_obj = solve_scs_direct(data, max_iters=max_iters)
     @show scs_direct_obj
-    
+
     print_timer()
     println()
 end
@@ -267,5 +498,34 @@ run_benchmark(num_facilities=5, num_customers=20, num_locations=10,
 run_benchmark(num_facilities=5, num_customers=20, num_locations=10,
     time_limit_sec=Inf, max_iters=10000)
 
-run_benchmark(num_facilities=10, num_customers=2000, num_locations=1000,
-    time_limit_sec=5, max_iters=10)
+run_benchmark(num_facilities=5, num_customers=20, num_locations=10,
+    time_limit_sec=0.0, max_iters=1)
+
+
+# run_benchmark(num_facilities=10, num_customers=2000, num_locations=1000,
+#     time_limit_sec=0.1, max_iters=1)
+
+# using ProfileView
+
+# function time_scs()
+#     num_facilities=10
+#     num_customers=2000
+#     num_locations=1000
+#     data = PMedianData(num_facilities, num_customers, num_locations, rand(num_customers) .* num_locations)
+#     cache = MOI.Utilities.CachingOptimizer(
+#         MOI.Utilities.UniversalFallback(MOI.Utilities.Model{Float64}()),
+#         SCS.Optimizer()
+#     )
+#     model = MOI.Bridges.full_bridge_optimizer(cache, Float64)
+#     MOI.Utilities.reset_optimizer(cache)
+#     params = [(MOI.RawParameter("max_iters"), 1),
+#         (MOI.Silent(), true),
+#         (MOI.RawParameter("acceleration_lookback"), 0)]
+#     for (param, value) in params
+#         MOI.set(model, param, value)
+#     end
+#     return model, data
+# end
+# GC.gc()
+# model, data = time_scs();
+# @profview generate_moi_problem(model, data);
